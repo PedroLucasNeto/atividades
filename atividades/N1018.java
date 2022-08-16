d5630a5da7df70805ebc2a39576f8581fc65759e
PedroLucasNeto <pedrolukasneto@gmail.com>
2022-08-16 19:51:27 -0300

    Dia 4

--- atividades/N1018.java
+++ atividades/N1018.java
@@ -8,10 +8,12 @@ public class N1018 {
 		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
 		int valor = scanner.nextInt();
+		
 		int notas[] = { 100, 50, 20, 10, 5, 2, 1 };
 		System.out.println(valor);
+		
 		for (int i = 0; i < notas.length; i++) {
-			int conta = valor / notas[i];
+			int conta = valor / notas[i]; 
 			System.out.printf("\n%d nota(s) de R$ %d ",conta, notas[i]);
 			valor = valor % notas[i];
 		}
--- atividades/N1021.java
+++ atividades/N1021.java
@@ -1,19 +1,29 @@
 package atividades;
+
+import java.util.Locale;
 import java.util.Scanner;
+
 public class N1021 {
-	public static void main (String [] args) {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		Scanner scanner = new Scanner(System.in);
-		//TODO Resolver questão 21;
-	/*	double entrada = scanner.nextDouble();
-		double notas[] = {100, 50, 20, 10, 5, 2};
-		for (int i =0; i <notas.length; i++) {
-			double resultadoNotas = entrada / notas[i];
-			entrada = entrada % notas[i];
-		System.out.println(resultadoNotas);	
-		
+		double notas[] = { 100, 50, 20, 10, 5, 2 };
+		double moedas[] = { 1, 0.50, 0.25, 0.10, 0.05, 0.01 };
+		double entrada = scanner.nextDouble();
+
+		for (int i = 0; i < notas.length; i++) {
+			double contaNotas = entrada / notas[i];
+			entrada %= notas[i];
+			System.out.printf("\n%.0f Notas de %.2f", Math.floor(contaNotas), notas[i]);
 		}
-		*/
-		
+		System.out.println("\nMOEDAS");
+
+		for (int c = 0; c < moedas.length; c++) {
+			double contaMoedas = entrada / moedas[c];
+			entrada %= moedas[c];
+			System.out.printf("\n%d Moedas de %.2f", (int) (contaMoedas), moedas[c]);
+		}
+		scanner.close();
 	}
 
 }
