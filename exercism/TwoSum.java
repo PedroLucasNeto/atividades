c4ff545a84e0c849e2b6c12d5877944d07b419f3
PedroLucasNeto <pedrolukasneto@gmail.com>
2022-08-17 22:58:37 -0300

    Dia 5

--- exercism/TwoSum.java
+++ exercism/TwoSum.java
@@ -5,16 +5,24 @@ import java.util.Scanner;
 public class TwoSum {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
 		String[] input = scanner.nextLine().split(" ");
 		double target = scanner.nextDouble();
+		System.out.println(metodo(input, target));
+
+		scanner.close();
+	}
+
+	private static String metodo(String[] input, double target) {
 		for (int i = 0; i < input.length; i++) {
 			for (int c = 1; c < input.length; c++) {
 				double sum = Double.parseDouble(input[i]) + (Double.parseDouble(input[c]));
 				if (sum == target) {
-					System.out.println(i + "," + c);
+					return i + "," + c;
 				}
 			}
 		}
-		scanner.close();
+		return "-1";
+
 	}
 }
